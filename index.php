c128e7a39b10ceb417f147051946e63a694e044e
MAITRE-ADJAYE <160837809+MAITRE-ADJAYE@users.noreply.github.com>
2024-05-04 17:09:57 +0100

    mettre à jour sur rites et clan

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site vitrine du peuple kanigui</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+        <div class="fade-in">
+            <h1>Histoite</h1>
+        </div>
+
+    <div class="contenair-paragraphe border">
+        <div class="fade-in-sous-titre">
+            <h4>Migration Bantou vers le centre de l'Afrique</h4>
+        </div>
+        <div class="contenair"> 
+            <div class="scale-in">
+                <a href="img/image-forgeron-2.jpeg"><img class="image" src="img/image-forgeron-2.jpg" alt="image"></a>
+            </div>
+                <p class="paragraphe slide">
+                La coutume du peuple Bakanigui, similaire à celle des Obambas, est imprégnée de rites et
+                    de traditions profondément ancrés dans leur société.
+                    Chaque clan Bakanigui possède ses propres forgerons, chargés d'extraire le fer du sol et de
+                    fabriquer divers outils et objets essentiels tels que marteaux, houes, sagaies, bracelets
+                    (utilisés comme monnaie), haches étroites et pipes. Des masques appelés "amboi" sont
+                    confectionnés à partir de cuivre provenant des neptunes. De plus, la poterie en terre cuite
+                    est également une pratique courante parmi eux.
+                </p>
+
+            <div class="scale-in">
+                <a  href="img/image-potier.jpeg"><img class="image col-4" src="img/image-potier.jpeg" alt="image-potier"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="contenair-paragraphe border">
+        <div class="fade-in-sous-titre">
+            <h4>Migration Bantou vers le centre de l'Afrique</h4>
+        </div>
+        <div class="contenair"> 
+            <div class="scale-in">
+                <a href="img/image-forgeron-2.jpeg"><img class="image" src="img/image-forgeron-2.jpg" alt="image"></a>
+            </div>
+                <p class="paragraphe slide">
+                En ce qui concerne l'agriculture, ils cultivent une variété de produits tels que le maïs (appelé "aloumo"), 
+                le manioc, les bananes, les ignames, les concombres et la canne à sucre. Ils pratiquent également la pêche 
+                et la chasse au filet pour subvenir à leurs besoins alimentaires. Les vêtements traditionnels sont fabriqués 
+                à partir d'écorce d'arbre, avec des pagnes ("obati") pour les hommes et des tenues composées de deux plaques 
+                avant et arrière pour les femmes. À une certaine époque, les Bakanigui fabriquaient également des pagnes en raphia,
+                sous l'influence de Brazza.
+                </p>
+
+            <div class="scale-in">
+                <a  href="img/image-potier.jpeg"><img class="image col-4" src="img/image-potier.jpeg" alt="image-potier"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="contenair-paragraphe border">
+        <div class="fade-in-sous-titre">
+            <h4>Migration Bantou vers le centre de l'Afrique</h4>
+        </div>
+        <div class="contenair"> 
+            <div class="scale-in">
+                <a href="img/image-forgeron-2.jpeg"><img class="image" src="img/image-forgeron-2.jpg" alt="image"></a>
+            </div>
+                <p class="paragraphe slide">
+                En ce qui concerne l'agriculture, ils cultivent une variété de produits tels que le maïs (appelé "aloumo"), l
+                e manioc, les bananes, les ignames, les concombres et la canne à sucre. Ils pratiquent également la pêche 
+                et la chasse au filet pour subvenir à leurs besoins alimentaires. Les vêtements traditionnels sont fabriqués 
+                à partir d'écorce d'arbre, avec des pagnes ("obati") pour les hommes et des tenues composées de deux plaques 
+                avant et arrière pour les femmes. À une certaine époque, les Bakanigui fabriquaient également des pagnes en raphia, 
+                sous l'influence de Brazza.
+                </p>
+
+            <div class="scale-in">
+                <a  href="img/image-potier.jpeg"><img class="image col-4" src="img/image-potier.jpeg" alt="image-potier"></a>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
